9e57206d298d7384de3baf9fcd9cd2d5a2504ee4
david.perren <david.perren05@gmail.com>
2022-04-20 21:45:48 -0300

    Primer commit, presentación del primer ejercicio, más practica

---
+++ index.js
@@ -0,0 +1,94 @@
+let numero = parseInt(prompt("Ingrese un número para generar la tabla de multiplicar"));
+
+for(let i = 1; i <=10; i++){
+    let multi = numero * i;
+    alert(numero + " x " + i + " = " + multi);
+}
+
+// Pedir nombre
+
+/* let nombre = prompt("Ingresa tu nombre");
+let salida  = "Hola" + " " + nombre + " " + "¿Como estás?";
+alert(salida); */ 
+
+//Sumar números
+
+// let numero1 = parseInt(prompt("Ingrese un número"));
+// let numero2 = parseInt(prompt("Ingrese otro número"));
+// let operacion = prompt("Ingrese una operacion");
+
+// if(operacion === "suma"){
+//     alert(numero1+numero2);
+// }else if (operacion === "resta"){
+//     alert(numero1-numero2);
+// }else if (operacion === "multiplicacion"){
+//     alert(numero1*numero2);
+// }else if(operacion === "division"){
+//     alert(numero1 / numero2);
+// } else {
+//     alert("Se esperaba una operación");
+// }
+
+
+
+/* var x = prompt("Enter a Value", "0");
+var y = prompt("Enter a Value", "0");
+var num1 = parseInt(x);
+var num2 = parseInt(y); */
+
+//ACTIVIDAD 1
+// let nombre = "Homero";
+// let apellido = "Simpson";
+// let edad = "39";
+
+// console.log(nombre);
+// console.log(apellido);
+// console.log(edad);
+
+// //ACTIVIDAD 2
+
+// const ciudad1 = "Rafaela";
+// const ciudad2 = "Sunchales";
+// const ciudad3 = "Esperanza";
+// const ciudad4 = "Santa fe";
+// const ciudad5 = "Rosario";
+
+// console.log(ciudad1);
+// console.log(ciudad2);
+// console.log(ciudad3);
+// console.log(ciudad4);
+// console.log(ciudad5);
+
+//ACTIVIDAD 3
+// const codigo = "B47U89RE243";
+// let nombreApellido = "Bart Simpson";
+// let direccion = "742 Evergreen Terr";
+// let ciudad = "Springfield, USA";
+// let nacimiento = "2/11/70";
+// let sexo = "M";
+
+// let carnet = "SPRINGFIELD DRIVE LICENCE" +
+// "CODIGO: " + codigo +
+// "NOMBRE Y APELLIDO: " + nombreApellido + 
+// "DIRECCIÓN: " + direccion + 
+// "CIUDAD: " + ciudad +
+// "NACIMIENTO: " + nacimiento +
+// "SEXO: " + sexo;
+
+// console.log(carnet);
+
+//ACTIVIDAD 4
+
+// let personajes = prompt("Nombra los 5 personajes de la familia Simpson, separados por una coma");
+
+// alert(personajes)
+
+//ACTIVIDAD 5
+
+// alert("el siguiente programa calcula el precio de un producto con un porcentaje aplicado");
+// alert("en la siguiente ventana, coloque un precio y a continuación coloque el porcentaje de descuento a aplicar");
+// let precio = parseInt(prompt("Coloque un precio"));
+// let descuento = parseInt(prompt("Coloque un porcentaje de descuento"));
+
+// let resultado = (precio - (precio * (descuento/100)));
+// alert(resultado);
