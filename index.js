c6ea6c282fb4a488c73a2b2459926ca7e12dcc87
david.perren <david.perren05@gmail.com>
2022-04-30 10:44:42 -0300

    Se resolvió Javascript 2

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 
-alert("El siguiente programa calcula el precio de un producto con un porcentaje aplicado");
-alert("En la siguiente ventana, coloque un precio y a continuación coloque el porcentaje de descuento a aplicar");
-let precio = parseInt(prompt("Coloque un precio"));
-let descuento = parseInt(prompt("Coloque un porcentaje de descuento"));
-
-let resultado = (precio - (precio * (descuento/100)));
-alert("El precio de su producto es " + resultado);
 
 
 // let numero = parseInt(prompt("Ingrese un número para generar la tabla de multiplicar"));
@@ -34,7 +27,7 @@ alert(salida); */
 // }else if (operacion === "multiplicacion"){
 //     alert(numero1*numero2);
 // }else if(operacion === "division"){
-//     alert(numero1 / numero2);
+    //     alert(numero1 / numero2);
 // } else {
 //     alert("Se esperaba una operación");
 // }
@@ -95,3 +88,96 @@ var num2 = parseInt(y); */
 
 //ACTIVIDAD 5
 
+// alert("El siguiente programa calcula el precio de un producto con un porcentaje aplicado");
+// alert("En la siguiente ventana, coloque un precio y a continuación coloque el porcentaje de descuento a aplicar");
+// let precio = parseInt(prompt("Coloque un precio"));
+// let descuento = parseInt(prompt("Coloque un porcentaje de descuento"));
+
+// let resultado = (precio - (precio * (descuento/100)));
+// alert("El precio de su producto es " + resultado);
+
+//JAVASCRIPT 2 
+//actividad 1
+
+// let nombre = prompt("Ingrese un nombre")
+
+// if (nombre === "David"){
+//     alert("Fui yo");
+// } else {
+//     alert("Yo no fui");
+// }
+
+//Actividad 2
+// let oprimeTecla = prompt("Oprime una tecla")
+
+// if (oprimeTecla == "y" || oprimeTecla == "Y"){
+//     alert("Correcto");
+// } else {
+//     alert("Incorrecto")
+// }
+
+//Actividad 3
+// let personaje = prompt("Menu\n Elige tu personaje:\n1: Homero\n2: Marge\n3: Bart\n4: Lisa")
+
+// if(personaje == 1){
+//     alert("Elegiste a homero")
+// } else if(personaje == 2){
+//     alert("Elegiste a Marge")
+// } else if(personaje == 3){
+//     alert("Elegiste a Bart")
+// } else if(personaje == 4){
+//     alert("Elegiste a Lisa")
+// } else {
+//     alert("No seleccionaste un personaje de la lista") 
+// }
+
+// let personaje = prompt("Menu\n Elige tu personaje:\n1: Homero\n2: Marge\n3: Bart\n4: Lisa")
+
+//Actividad 4
+// let presupuesto = prompt("Colocar un numero para que indique el presupuesto") 
+
+// if(presupuesto == 0 && presupuesto <= 1000){
+//     alert("Presupuesto Bajo")
+// } else if(presupuesto > 1000 && presupuesto <= 3000){
+//     alert("Presupuesto medio")
+// } else if(presupuesto > 3000){
+//     alert("Presupuesto alto")
+// } else if(presupuesto < 0){
+//     alert("Tu presupuesto no puede ser negativo")
+// } else {
+//     alert("No colocaste un número") 
+// }
+
+//Actividad 5
+// alert("Colocar 4 productos a continuación")
+// let producto1 = prompt("Producto 1")
+// let producto2 = prompt("Producto 2")
+// let producto3 = prompt("Producto 3")
+// let producto4 = prompt("Producto 4")
+
+// let productos = ((producto1) + "," + (producto2) + "," + (producto3) + "," + (producto4))
+
+// if ((producto1 != "") && (producto2 != "") && (producto3 != "") && (producto4 != "")){
+//     alert("Sus productos son: "+ productos)
+// } else {
+//     alert("Tus productos estan vacios")
+// }
+
+// let producto1 = prompt('INGRESAR 1er PRODUCTO');
+// let producto2 = prompt('INGRESAR 2do PRODUCTO');
+// let producto3 = prompt('INGRESAR 3er PRODUCTO');
+// let producto4 = prompt('INGRESAR 4to PRODUCTO');
+
+// if ((producto1 != "") && (producto2 != "") && (producto3 != "") && (producto4 != "")) {
+//     let productos = "1) " + producto1 + " " +
+//         "2) " + producto2 + " " +
+//         "3) " + producto3 + " " +
+//         "4) " + producto4;
+//     alert(productos);
+// } else {
+//     alert("ERROR: ES NECESARIO CARGAR TODOS LOS PRODUCTOS");
+// }
+
+
+//Javascript 3
+//Actividad 1
